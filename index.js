b1682fe04613fe702bdb6c24195be377200d71bd
Jakedalus <jacquedalus@gmail.com>
2021-05-08 19:38:54 -0400

    Create users route to GET all users

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const authRouter = require('./routes/auth');
 const coursesRouter = require('./routes/courses');
+const usersRouter = require('./routes/users');
 const middleware = require('./utils/middleware');
 
 mongoose.connect(process.env.MONGODB_URI, {
@@ -22,6 +23,7 @@ app.use(middleware.tokenExtractor);
 
 app.use('/api/auth', authRouter);
 app.use('/api/courses', coursesRouter);
+app.use('/api/users', usersRouter);
 
 // app.get('/', (req, res) => {
 // 	res.send('<h1>Hello World!!</h1>');
