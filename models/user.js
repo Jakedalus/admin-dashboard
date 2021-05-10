f570e7290cb62dbcae3d08e39326a0f80347c178
Jakedalus <jacquedalus@gmail.com>
2021-05-10 18:31:33 -0400

    Add more info to user model

--- models/user.js
+++ models/user.js
@@ -3,14 +3,22 @@ const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = new mongoose.Schema(
 	{
-		username     : {
+		username       : {
 			type      : String,
 			minlength : 3,
 			required  : true,
 			unique    : true
 		},
-		passwordHash : String,
-		courses      : [
+		passwordHash   : String,
+		userType       : String,
+		name           : String,
+		email          : String,
+		gender         : String,
+		age            : Number,
+		educationLevel : String,
+		nativeLang     : String,
+		englishLevel   : String,
+		courses        : [
 			{
 				type : mongoose.Schema.Types.ObjectId,
 				ref  : 'Course'
