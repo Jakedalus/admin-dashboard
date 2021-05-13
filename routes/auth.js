2240262408b42c5d3ded58d5e7cc9f62044b9280
Jakedalus <jacquedalus@gmail.com>
2021-05-13 18:38:48 -0400

    Add changepassword auth PUT route, fix savedUser error in singup route

--- routes/auth.js
+++ routes/auth.js
@@ -46,7 +46,6 @@ authRouter.post('/signup', async (req, res) => {
 		saltRounds
 	);
 
-	// try {
 	const user = new User({
 		username,
 		passwordHash,
@@ -63,12 +62,16 @@ authRouter.post('/signup', async (req, res) => {
 
 	const token = jwt.sign(userForToken, process.env.SECRET);
 
-	// console.log(`savedUser`, savedUser);
+	console.log(`savedUser`, savedUser);
 
-	res.status(200).json({ ...savedUser, token });
-	// } catch (error) {
-	// 	res.status(400).json({ error });
-	// }
+	res.status(200).json({
+		username  : savedUser.username,
+		name      : savedUser.name,
+		email     : savedUser.email,
+		createdAt : savedUser.createdAt,
+		_id       : savedUser._id,
+		token
+	});
 });
 
 authRouter.post('/signin', async (req, res) => {
@@ -104,6 +107,56 @@ authRouter.post('/signin', async (req, res) => {
 		name,
 		email
 	});
+});
+
+authRouter.put('/changepassword', async (req, res) => {
+	const { username, password, newPassword } = req.body;
+
+	if (!username || !newPassword) {
+		res.status(400).json({
+			error : 'must include username and password'
+		});
+	}
+
+	if (username.length < 3 || newPassword.length < 8) {
+		res.status(400).json({
+			error : 'username and/or password is not long enough'
+		});
+	}
+
+	const user = await User.findOne({
+		username
+	});
+
+	const passwordCorrect =
+		user === null
+			? false
+			: await bcrypt.compare(password, user.passwordHash);
+
+	if (!user || !passwordCorrect) {
+		return res.status(401).json({
+			error : 'invalid password'
+		});
+	}
+
+	const saltRounds = 10;
+	const passwordHash = await bcrypt.hash(
+		newPassword,
+		saltRounds
+	);
+
+	await User.findByIdAndUpdate(user.id, { passwordHash });
+
+	// const userForToken = {
+	// 	username,
+	// 	id       : user._id
+	// };
+
+	// const token = jwt.sign(userForToken, process.env.SECRET);
+
+	// const { name, email } = user;
+
+	res.status(200).end();
 });
 
 module.exports = authRouter;
